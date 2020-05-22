a2518fe25de53146366d8ef2a6209d9e4df9455f
glopezi <glopezi@minsait.com>
2020-05-23 00:01:02 +0200

    Updates

---
+++ precache-manifest.e34c687580b02fd615bbfe8ac06ccca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a1e800b55ed0df5f217b5034701ce7",
+    "url": "/crucigrama/index.html"
+  },
+  {
+    "revision": "8232ce0a9fe528dd2c26",
+    "url": "/crucigrama/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "33ea0335b0f09785c6e7",
+    "url": "/crucigrama/static/js/2.a17b1d4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crucigrama/static/js/2.a17b1d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8232ce0a9fe528dd2c26",
+    "url": "/crucigrama/static/js/main.08c125e9.chunk.js"
+  },
+  {
+    "revision": "9ec8f8a81b355953c3e6",
+    "url": "/crucigrama/static/js/runtime-main.d8cfc954.js"
+  }
+]);
